b7cace85ce729900475e366e80c79094a437d6d2
alok1412 <bcasprabhatjoshi@gmail.com>
2020-11-25 03:18:30 +0530

    basic react finished

--- src/index.js
+++ src/index.js
@@ -4,35 +4,24 @@ import ReactDom from 'react-dom'
 //CSS
 import './index.css'
 
+//named import
+import {books} from './books'
+
+//default import we can name book as anything we like
+import Book from './Book.js'
+
 //capatalize the first word of function to let react know it is a component
 function BookList() {
   return (
   <section className="booklist">
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
+    {books.map((book) => {
+      return(
+        <Book key={book.id} {...book}></Book>
+
+      );
+    })}
   </section>
   );
   }
-
-//Not a good way of rendering because of readabilityand back to back function calls to React.createElement for children.
-// const Name = () => {
-//   return React.createElement('h1',{},'Hello World!')
-// }
-
-const Book = () => {
-  return (
-    <article className="book">
-      <Image/>
-      <Title/>
-      <Author/>
-    </article>
-  )
-}
-
-const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/41X8Oe3zTsL._AC_SX184_.jpg" alt="table_photo"/>
-const Author = () => <h4>Me</h4>
-const Title = () => <h4 style={{color:'#617d98',fontSize:'0.75rem',marginTop:"20px"}}>how to study</h4>
 //now use ReactDom to render the component 
 ReactDom.render(<BookList />, document.getElementById('root'));
